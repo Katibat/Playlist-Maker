7dba28aa1a846cc6e3cddb5c4919f074dedba209
Kate Mahova <kate.mahova@yandex.ru>
2023-11-22 13:24:43 +0300

    feat: single Toolbar for fragments into BottomNavigationView

--- app/src/main/java/com/example/playlistmaker/root/ui/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/root/ui/RootActivity.kt
@@ -3,6 +3,7 @@ package com.example.playlistmaker.root.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityRootBinding
@@ -19,14 +20,16 @@ class RootActivity : AppCompatActivity() {
         findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 
-
-
         // Toolbar
-//        val navController = findNavController()
-//        val appBarConfiguration = AppBarConfiguration(navController.graph)
-//
-//        view.findViewById<Toolbar>(R.id.toolbar)
-//            .setupWithNavController(navController, appBarConfiguration)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding?.toolbar?.setupWithNavController(navController, appBarConfiguration)
+        // скрыть стрелку назад, через supportActionBar с флагом false не работает
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if ((destination.id == R.id.searchFragment) ||
+                (destination.id == R.id.settingsFragment)) {
+                binding?.toolbar?.navigationIcon = null
+            }
+        }
 
         binding?.bottomNavigationView?.setupWithNavController(navController)
     }
